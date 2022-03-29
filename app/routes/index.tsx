5f747dd46a3919e977bc85fc12c95c16094d1cbb
3x071c <dev@3x071c.com>
2022-03-29 18:01:35 +0200

    feat: initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,40 @@
+import { Magic } from "@magic-sdk/admin";
+import { LoaderFunction, json } from "remix";
+
+export const loader: LoaderFunction = () => {
+	const m = new Magic("sk_live_insert-secret-key-here");
+	return json({});
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
